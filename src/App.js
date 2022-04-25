6f0e2cd289cefb987a0eb8d8897e6bdedd1e5d00
Joyce Aviles <joyce.aviles@alvaria.com>
2022-04-25 10:37:17 +0800

    Added initial login screen

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import React, {Component} from 'react'
-import Table from './Table'
-import Form from './Form'
+import React from 'react'
+import SignInSide from './signIn'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-class App extends Component {
-  state = {
-      characters: []
-  }
-
-  removeCharacter = (index) => {
-    const {characters} = this.state
-  
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
-  }
-
-  handleSubmit = (character) => {
-    this.setState({characters: [...this.state.characters, character]})
-  }
-
-  render() {
-    const {characters} = this.state;
-    return (
-        <div className="container">
-            <Table characterData={characters} removeCharacter={this.removeCharacter} />
-            <Form handleSubmit={this.handleSubmit} />
-            
-        </div>
-    )    
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <SignInSide />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
